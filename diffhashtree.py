bad23f4658f33c5963be6baa507c12054159c042
root <paul@cummings.so>
2017-05-01 21:05:45 +0000

    diffhashtree with tar

--- diffhashtree.py
+++ diffhashtree.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
 import os, hashlib, json, sys
-
-if len(sys.argv) != 3:
+from subprocess import call
+missinglist = ""
+if len(sys.argv) < 3:
         print "Syntax: xxxxxx.py <source file.json> <dest file.json>"
         sys.exit(0)
 else:
         srcfile = sys.argv[1]
 	dstfile = sys.argv[2]
+
+if len(sys.argv) == 4:
+	tgzfile = sys.argv[3]
+
 
 with open(srcfile) as sf:
     shashdict = json.loads(sf.read())
@@ -15,9 +20,20 @@ with open(srcfile) as sf:
 with open(dstfile) as df:
     dhashdict = json.loads(df.read())
 
+
+
 for key in shashdict:
 	if key in dhashdict:
 		found=True
 	else:
-		print "Missing: " + shashdict[key]
+		missinglist += shashdict[key] + "\n"
+
+print "Missing:"
+print missinglist
+
+if len(sys.argv) == 4:
+	with open("tarfilelist.txt","w") as outfile:
+        	outfile.write(missinglist)
+	call(["tar", "-cvzf", tgzfile, "-T", "tarfilelist.txt"])
+
 
